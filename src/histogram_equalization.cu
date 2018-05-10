89a97285a79bd7f6ed7d4ab2211197a0f7ad6532
rpinheiro033 <rj.pinheiro@campus.fct.unl.pt>
2018-05-10 05:14:30 +0100

    tryryreyery

--- src/histogram_equalization.cu
+++ src/histogram_equalization.cu
@@ -33,61 +33,61 @@ int main(int argc, char **argv) {
 
   /* parse the input arguments */
   wbImage_t inputImage = wbImport(argv[1]);
-  
+
   int imageWidth    = wbImage_getWidth(inputImage);
   int imageHeight   = wbImage_getHeight(inputImage);
   int imageChannels = wbImage_getChannels(inputImage);
-  
+
   //step 1
   float *imageData = wbImage_getData(inputImage);
   float *d_imageData;
-  
+
   unsigned char *ucharImage;
   unsigned char *d_ucharImage;
-  
+
   int heightPerWidth = imageWidth * imageHeight;
   int max = heightPerWidth * imageChannels;
-  
+
   int ucharImageSize = max * sizeof(unsigned char);
   int imageFloatSize = max * sizeof(float);
-  
+
   cudaMalloc((void**)&d_imageData, imageFloatSize);
   cudaMalloc((void**)&d_ucharImage, ucharImageSize);
-  
+
   ucharImage = (unsigned char *)malloc(ucharImageSize);
-  
+
   cudaMemcpy(d_imageData, imageData, imageFloatSize, cudaMemcpyHostToDevice);
-  
+
   vectorAdd<<<max/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_imageData, d_ucharImage, max);
-  
+
   cudaMemcpy(d_ucharImage, ucharImage, ucharImageSize, cudaMemcpyHostToHost);
-  
+
   free(imageData);
   cudaFree(d_imageData);
-  
+
   //step 2
   unsigned char *grayImage;
   unsigned char *d_grayImage;
-  
+
   int grayImageSize = heightPerWidth * sizeof(unsigned char);
-  
+
   cudaMalloc((void**)&d_grayImage, grayImageSize);
-  
+
   grayImage = (unsigned char *)malloc(grayImageSize);
-  
-  greyScaleTransf<<<heightPerWidth/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_ucharImage, d_grayImage, heightPerWidth)
-  
+
+  greyScaleTransf<<<heightPerWidth/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_ucharImage, d_grayImage, heightPerWidth);
+
   cudaMemcpy(d_grayImage, grayImage, grayImageSize, cudaMemcpyHostToHost);
-  
-  
+
+
   free(ucharImage);
   free(grayImage);
-  
+
   cudaFree(d_ucharImage);
   cudaFree(d_grayImage);
-  
-  
-  
-  
+
+
+
+
   return 0;
 }
